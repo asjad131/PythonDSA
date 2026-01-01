2738cfef1a36e0ed00736b8ebd0e8f09de615abc
asjad131 <asjad131@gmail.com>
2026-01-01 17:06:32 +0530

    start datatypes

---
+++ 02_datatypes/chapter_2.py
@@ -0,0 +1,7 @@
+spice_mix = set()
+print(f"Initial Spice Mix ID: {id(spice_mix)}")
+print(f"Initial Spice Mix : {(spice_mix)}")
+spice_mix.add("Ginger")
+spice_mix.add("Cardamom")
+print(f"After Spice Mix ID: {id(spice_mix)}")
+print(f"After Spice Mix : {(spice_mix)}")
