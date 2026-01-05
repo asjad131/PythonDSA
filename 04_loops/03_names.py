8817f666a45f1024cf75cf8b2aa5c76aefa63684
asjad131 <asjad131@gmail.com>
2026-01-05 23:15:24 +0530

    add loops

---
+++ 04_loops/03_names.py
@@ -0,0 +1,4 @@
+names = ["Asjad", "Ben", "Charlie", "Doglas", "Emily"]
+
+for name in names:
+    print(f" Preapring next tea for :  {name}")
---
+++ 04_loops/04_chai_menu.py
@@ -0,0 +1,4 @@
+menu = ["Green", "Lemon", "Spiced", "Mint", "Milk"]
+
+for idx, item in enumerate(menu, start=1):
+    print(f"{idx} : {item} chai")
